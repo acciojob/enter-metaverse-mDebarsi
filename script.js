c1495a719c59cd0a7b1522f0488b29a7d0ef1e3e
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-11-28 20:38:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-//your JS code here. If required.
-let para=document.getElementById("status");
-enterBtn.addEventListener("click",function () {
-
-	let heading=document.createElement("h1");
-	heading.innerText=para.innerText;
-
-	para.innerText="";
-	para.appendChild(heading);
-});
+let para=document.getElementById('status');
+  enterBtn.addEventListener("click",function(){
+	  let heading=document.createElement("h1");
+	  heading.innerText="Entered Metaverse";
+	  para.innerText="";
+	  para.appendChild(heading);
+  })
